fef18648d610e08472cb260e56a62f182c1c2fff
Dennis Varghese <dennis.varghese@hotmail.com>
2025-09-05 23:21:32 -0400

    feat: add setTimeout() function for button clicking

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ function printReceipt() {
 	r.innerHTML = o
 	r.innerHTML += `<br>total: $${t.toFixed(2)}`
 }
+
+let buttons = document.querySelectorAll('button')
+
+for (let b of buttons) {
+	b.addEventListener('click', function() {
+		this.disabled = true
+
+		setTimeout(() => {
+			this.disabled = false
+		}, 200)
+	})
+}
